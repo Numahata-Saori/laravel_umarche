c188a9a98c1ba95ecc64aa519fc7651c6c164c50
Numahata <numahata.saori@gmail.com>
2022-08-15 00:20:19 +0900

    add Breeze

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
@@ -8,4 +8,8 @@ export default defineConfig({
             'resources/js/app.js',
         ]),
     ],
+    server: {
+        https: true,
+        host: 'localhost',
+    },
 });
